c411b2f99329a5bf9fcee80dd3958db10b9cb282
jeanmax1me <jeanmaximegilbert@gmail.com>
2024-03-05 11:19:30 +0100

    remove webvitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
 import "./css/global.css"
 import AboutPage from './pages/aboutpage';
 import ErrorPage from './pages/errorpage';
@@ -42,7 +41,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+
